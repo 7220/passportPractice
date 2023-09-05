0a2a19c0a495a430d143d40aaf8cf4b5cbb15d63
7220 <kumasaki@naver.com>
2023-09-06 00:26:29 +0900

    get, post

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
+const secretText = 'superSecret';
 
-app.request(express.json());
+const posts = [
+  {
+    username: 'John',
+    title: 'Post 1'
+  },
+  {
+    username: 'Han',
+    title: 'Post 2'
+  }
+]
+
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const user = { name: username};
+
+  const accessToken = jwt.sign(user, secretText);
+  res.json({ accessToken: accessToken })
+})
+
+app.get('/posts', (req, res) => {
+  res.json(posts);
+})
 
 const port = 4000;
 app.listen(port, () => {
